c3ce96bbebb7c57e330a2b44ac739492c2e2314f
yuanlong <yuanlong@rivamed.cn>
2022-07-29 18:41:45 +0800

    code generator tools

---
+++ mybatisgeneratortools/src/test/java/com/example/mybatisgeneratortools/MybatisgeneratortoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisgeneratortools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisgeneratortoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
